1db081a5571997910f2ddd0aa70c083da79cc67c
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-10-05 08:59:02 -0700

    update style

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,43 +1,49 @@
-import GoodLoopLogo from './assets/goodloop_logo.svg';
 import SignupForm from './SignupForm';
+import GoodloopLogo from './assets/goodloop_logo.svg';
+import conesSrc from "./assets/cones.png";
+import familySrc from "./assets/family.png";
+import shieldSrc from './assets/shield.png';
+import rocketSrc from "./assets/rocketship.png";
+
+import mockScreen from './assets/mockscreen.png';
 
 // Logo
 const HeaderLogo = () => (
   <>
     <div className="w-24 h-24 mb-2 bg-gradient-to-br from-amber-400 to-amber-500 rounded-3xl shadow-[0px_9.600000381469727px_14.40000057220459px_-9.600000381469727px_rgba(253,230,138,1.00)] shadow-[0px_24px_36px_-7.200000286102295px_rgba(253,230,138,1.00)] inline-flex justify-center items-center overflow-hidden">
       <div className="w-14 h-14 relative">
-        <img className="w-100 absolute" src={GoodLoopLogo} alt="GoodLoop logo" />
+        <img className="w-100 absolute" src={GoodloopLogo} alt="Goodloop logo" />
       </div>
     </div>
     <h2 className='logo-text'>Goodloop</h2>
   </>
 );
 
+const MockScreen = () => {
+  return <div className='mockup'>
+    <img src={mockScreen}/>
+    <div className='mockbtn bg-sky-400'/>
+  </div>
+}
+
 // Hero Section
 const HeroSection = () => (
   <section className="hero">
 
     <div className="pill">Coming soon to iOS & Android</div>
-    <h1>Your trusted caregiving network</h1>
+    <h1>Find your network, thrive together</h1>
 
     <div className='bg-blur'/>
 
     <p>
-      Connect with verified neighbors for childcare, carpooling, and more. 
-      All in one seamless, community-first platform.
+      Work and family shouldn't be a tradeoff. 
+      Goodloop helps you save time and stress less by connecting you with trusted friends and neighbors for carpools and logistic challenges.
     </p>
     <p>
-      Be the first to try our services by joining our waitlist.
+      Join our waitlist to be the first to try our services.
     </p>
     <SignupForm />
-
-  {
-    // TODO: add screenshot of mockup
-  }
-    <div className='mockup'>
-      mocked UX design display
-    </div>
-    
+    <MockScreen />
   </section>
 );
 
@@ -49,69 +55,118 @@ type ServiceCardProps = {
 }
 
 // Service Card
-const ServiceCard = ({ emoji, title, subtitle, description }: ServiceCardProps) => (
-  <div className="flex w-full max-w-sm bg-gradient-to-r from-yellow-100 to-sky-100 rounded-xl p-[1px]">
-    <div className="bg-white rounded-xl border border-gray-200 p-6 flex flex-col gap-6">
-      <div className="flex items-start gap-4">
-        <div className="w-14 h-14 bg-sky-100 rounded-lg flex items-center justify-center text-2xl">
-          {emoji}
-        </div>
-        <div>
-          <h3 className="text-lg font-semibold text-gray-900">{title}</h3>
-          <p className="text-sm text-gray-500">{subtitle}</p>
+const ServiceCard = ({ emoji, title, subtitle, description }: ServiceCardProps) => {
+
+  const [time, helpers] = subtitle.split(" | ");
+
+  return (
+    <div className='flex flex-col w-full max-w-sm bg-gradient-to-r from-yellow-100 to-sky-100 rounded-xl p-[2px]'>
+    <div className='bg-white rounded-xl border border-gray-200 p-6 flex flex-col gap-6 flex-grow'>
+      <div className='service-card-header'>
+        <div className="service-card-emoji bg-sky-100">{emoji}</div>
+        <div className='service-card-header-content'>
+          <div className='text-lg font-semibold text-gray-900'>{title}</div>
+          <div className='text-sm text-gray-500'>{time}</div>
+          <div className='text-sm text-gray-500'>{helpers}</div>
         </div>
       </div>
-      <p className="text-sm text-gray-600">{description}</p>
+      <p className="text-sm text-gray-600">
+        {
+          description
+        }
+      </p>
     </div>
+    
   </div>
-);
+  )
+}
 
 // Services Section
 const ServicesSection = () => (
   <section className="services">
     <h2 className="text-5xl font-light text-gray-800">
-      Carpooling and family care made easier.
+      Carpools first, community forever.
     </h2>
-    <p>
-      Share carpooling responsibilities to make time for a better future
-    </p>
-
-    <div className="flex gap-8 justify-center">
+    <div className="service-card-list">
       <ServiceCard
         emoji="🏫"
-        title="School Run Carpool"
-        subtitle="Today, 3:00 PM • 3 trusted neighbors available"
-        description="Late for a meeting? Connect with verified neighbors who are already heading that way!"
-      />
-      <ServiceCard
-        emoji="🩰"
-        title="Dancing Class Carpool"
-        subtitle="This evening • 5 trusted neighbors available"
-        description="Need flexibility with your daughter's dance schedule? Your trusted network makes it simple. Give rides when you can, receive help when you need it."
+        title="School Carpool"
+        subtitle="Today, 3:00 PM | 3 trusted neighbors available"
+        description="Running late? Your circle has pickup covered."
       />
       <ServiceCard
         emoji="⚽️"
-        title="Soccer Practice Carpool"
-        subtitle="Today, 5:00 PM • 2 trusted neighbors available"
-        description="Juggling work and practice? Trusted friends nearby are ready to support."
+        title="Dancing Class Carpool"
+        subtitle="Today, 5:00 PM | 3 trusted neighbors available"
+        description="Practice made easy with trusted parents."
+      />
+      <ServiceCard
+        emoji="🩰"
+        title="Dance Class Carpool"
+        subtitle="Tomorrow, 4:00 PM | 4 trusted neighbors available"
+        description="Give rides when you can. Get help when you need."
       />
     </div>
   </section>
 );
 
-// Testimonials Section
-// const TestimonialsSection = () => (
-//   <section className="py-16 flex flex-col items-center gap-12">
-//     <div className="max-w-2xl text-center flex flex-col gap-4">
-//       <span className="px-6 py-2 bg-gray-100/30 rounded-full border border-gray-200 text-xs shadow-sm">
-//         Testimonials
-//       </span>
-//       <h2 className="text-5xl font-light">
-//         “Lorem ipsum dolor sit amet, consectetur adipiscing elit.”
-//       </h2>
-//     </div>
-//   </section>
-// );
+type Value = {
+  text: string;
+  src: string;
+  alt: string;
+}
+
+const ValuesSection = () => {
+  return <section className='values'>
+    <h2 className='text-5xl'>Our values</h2>
+    <ul className='flex flex-wrap'>
+      {
+        [
+          {
+            text: "We commit to the safety of families.",
+            src: shieldSrc,
+            alt: "shield"
+          },
+          {
+            text: "We break down barriers to allow families to thrive.",
+            src: conesSrc,
+            alt: "traffic cones"
+          },
+          {
+            text: "We treat our team and partners like family.",
+            src: familySrc,
+            alt: "family"
+          },
+          {
+            text: "We are future-focused to cultivate community for generations to come.",
+            src: rocketSrc,
+            alt: "rocket"
+          }
+        ].map((value: Value) => <li key={value.text}>
+          <div className="value-list-item-icon-container">
+            <img className="value-list-item-icon" src={value.src} alt={value.alt} />
+          </div>
+          {value.text}
+        </li>)
+      }
+    </ul>
+  </section>
+}
+
+const QuotesSection = () => {
+  return <section className='quotes-section'>
+    <div className="quotes-container">
+      <i className="quote-1">"My meeting is running late, but I need to pickup my kid.”</i>
+      <i className="quote-2">"I want to help other parents, but I need a simple way to connect."</i>
+      <i className="quote-3">"Driving in five directions at once isn't sustainable. There must a better way."</i>
+    </div>
+    <h2 className="text-5xl font-light text-gray-800 pb-3">Sound familiar?</h2>
+    <p>
+      Work-life balance is hard. Parents need trusted backup when plans shift. <br/>
+      Goodloop is building a modern solution for families who want to grow their networks of support.
+    </p>
+  </section>
+}
 
 const SignupFormSection = () => {
   return (
@@ -122,7 +177,7 @@ const SignupFormSection = () => {
         </h2>
         <p>
           It is our mission to support families and build community. <br/>
-          Joining the waitlist grants you first opportunity for optimizing your schedules for balancing worklife and family care.
+          Joining the waitlist grants you first opportunity for optimizing your schedules to balance worklife and family care.
         </p>
       </div>
       
@@ -139,7 +194,8 @@ export default function LandingPage() {
         <HeaderLogo />
         <HeroSection />
         <ServicesSection />
-        {/* <TestimonialsSection /> */}
+        <ValuesSection />
+        <QuotesSection />
         <SignupFormSection />
       </div>
       <footer>
--- src/SignupForm.tsx
+++ src/SignupForm.tsx
@@ -4,33 +4,45 @@ export default function SignupForm() {
   const [emailInput, setEmailInput] = useState("");
   const [nameInput, setNameInput] = useState("");
   const [zipcodeInput, setZipcodeInput] = useState("");
-  const [status, setStatus] = useState("");
 
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
     event.preventDefault();
 
-    // TODO: send email to db
-    console.log("form data", emailInput, nameInput, zipcodeInput);
-    setEmailInput("");
-    setNameInput("");
-    setZipcodeInput("");
-    setStatus("Added!");
+    const dataBody = JSON.stringify({
+      name: nameInput,
+      email: emailInput,
+      zipcode: zipcodeInput,
+      message: ""
+    })
 
-    setTimeout(() => setStatus(""), 2000);
+    try {
+      const url = 'https://goodloop-nexus.onrender.com/goodloop-mgmt-api/v1/waiting-lists/';
+      const resp = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: dataBody
+      });
+
+      if (resp.ok) {
+        console.log("Entry added.")
+        setEmailInput("");
+        setNameInput("");
+        setZipcodeInput("");
+      } else {
+        throw new Error(`Status: ${resp.status}. Failed to add entry.`);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+    
   }
 
-  return <div>
-    <form className="signup-form" onSubmit={handleSubmit}>
-      <input required name="name" value={nameInput} type="text" placeholder='Name' onChange={(e) => setNameInput(e.target.value)} />
-      <input required name="zipcode" value={zipcodeInput} type="zipcode" placeholder='Zip code' onChange={(e) => setZipcodeInput(e.target.value)} />
-      <input required name="email" value={emailInput} type="email" placeholder='Email' onChange={(e) => setEmailInput(e.target.value)} />
-      <button className="bg-sky-400 submit-btn" type="submit">Get notified</button>
-    </form>
-    <p className='h-5 mt-3'>
-      {
-        status
-      }
-    </p>
-  </div>
-  
+  return <form className="signup-form" onSubmit={handleSubmit}>
+    <input required name="name" value={nameInput} type="text" placeholder='Name' onChange={(e) => setNameInput(e.target.value)} />
+    <input required name="zipcode" value={zipcodeInput} type="tel" pattern="[0-9]*" placeholder="Zip code" max="99999" onChange={(e) => setZipcodeInput(e.target.value)} />
+    <input required name="email" value={emailInput} type="email" placeholder='Email' onChange={(e) => setEmailInput(e.target.value)} />
+    <button className="bg-sky-400 submit-btn" type="submit">Get notified</button>
+  </form>
 }
