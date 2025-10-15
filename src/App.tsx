276b2172b11291a64718ca9c90a8df5d427e5ee1
chiyobi <155046495+chiyobi@users.noreply.github.com>
2025-10-15 12:36:41 -0700

    test share api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import LandingPage from "./LandingPage";
 import './App.css';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 // WICG Spec: https://wicg.github.io/ua-client-hints
 type Navigator = NavigatorUA
@@ -45,29 +45,53 @@ interface NavigatorUAData extends UALowEntropyJSON {
     toJSON(): UALowEntropyJSON;
 }
 
-async function getBrowserInfo() {
-  const navig = window.navigator as Navigator;
-  if (navig.userAgentData) {
-    const uaData = navig.userAgentData;
-    console.log("Brand:", uaData.brands); // Array of browser brands and versions
-    console.log("Mobile:", uaData.mobile); // Boolean indicating mobile device
-    console.log("Platform:", uaData.platform); // Operating system
-    
-    const highEntropyValues = await uaData.getHighEntropyValues(["architecture", "model", "platformVersion", "uaFullVersion"]);
-    console.log("Full Version:", highEntropyValues.uaFullVersion);
-    console.log("Platform Version:", highEntropyValues.platformVersion);
-  } else {
-    console.log("User-Agent Client Hints API not supported.");
-  }
-}
-
 const App = () => {
+
+  const [browserInfo, setBrowserInfo] = useState<NavigatorUAData | null>(null); 
+
+  async function getBrowserInfo() {
+    const navig = window.navigator as Navigator;
+    if (navig.userAgentData) {
+      const uaData = navig.userAgentData;
+      console.log("Brand:", uaData.brands); // Array of browser brands and versions
+      console.log("Mobile:", uaData.mobile); // Boolean indicating mobile device
+      console.log("Platform:", uaData.platform); // Operating system
+
+      setBrowserInfo(uaData);
+    } else {
+      console.log("User-Agent Client Hints API not supported.");
+    }
+  }
 
   useEffect(() => {
     getBrowserInfo();
   }, [])
 
+  const navToApp = async () => {
+
+    const shareData = {
+      title: "Test title",
+      text: "test text",
+      url: "goodloop://",
+    };
+
+    try {
+      await navigator.share(shareData);
+      console.log("Shared successfully");
+    } catch (err) {
+      console.error(`Error: ${err}`);
+    }
+  }
+
   return <>
+  {
+    browserInfo && browserInfo.mobile && <button style={{
+      position: "absolute",
+      height: "10rem",
+      width: "10rem",
+      backgroundColor: "red"
+    }} onClick={navToApp}>Test</button>
+  }
    <LandingPage />
   </>
 }
